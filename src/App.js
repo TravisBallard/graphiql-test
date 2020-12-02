122b5f37034ef83c31c2ab7ae86469f47dacac83
Travis Ballard <travis.ballard@gmail.com>
2020-12-02 12:29:00 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {GraphiQL} from 'graphiql'
+import 'graphiql/graphiql.css'
 
+/**
+ * Fetcher for GraphiQL
+ * @param query
+ * @param variables
+ * @returns {Promise<never>|Promise<Response | *>}
+ */
+const gqlFetcher = ({query, variables = {}}) => {
+  if (!query || typeof query === 'undefined') return Promise.reject('No query.')
+
+  return fetch(`https://countries.trevorblades.com/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    })
+  })
+    .then(response => response.json())
+    .catch(response => response.text)
+}
+
+/**
+ * Default query to fill form with.
+ * @type {string}
+ */
+const defaultQuery = `
+  query getCountries {
+    countries{
+      code
+      name
+      continent{
+        name
+      }
+    }
+  }
+  
+  query getLanguages {
+    languages {
+      code
+      name
+      native
+      rtl
+    }
+  }
+`
+
+/**
+ * Display GraphiQL
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh'}}>
+      <GraphiQL fetcher={gqlFetcher} defaultQuery={defaultQuery} />
     </div>
   );
 }
